51ad0dd7fe8c4991507df8cd99b2c9cc883e8ba7
BAYUEQI <zouqi050813@gmail.com>
2026-03-10 10:47:03 +0800

    删除社交和动漫分类，重新实现平台按钮图标设置

--- app/src/main/java/com/samyak/repostore/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/samyak/repostore/ui/fragment/SearchFragment.kt
@@ -173,6 +173,8 @@ class SearchFragment : Fragment() {
         // Platforms chip
         binding.chipHasApk.text = "平台"
         binding.chipHasApk.setChipIconResource(R.drawable.ic_platform)
+        binding.chipHasApk.isCheckedIconVisible = false
+        binding.chipHasApk.chipIconVisible = true
         binding.chipHasApk.setOnClickListener {
             showPlatformsDialog()
         }
@@ -299,6 +301,7 @@ class SearchFragment : Fragment() {
         binding.chipHasApk.isChecked = platforms.isNotEmpty()
         binding.chipHasApk.setChipIconResource(R.drawable.ic_platform)
         binding.chipHasApk.isCheckedIconVisible = false
+        binding.chipHasApk.chipIconVisible = true
     }
 
     private fun updateChipStates(filters: SearchFilters) {
